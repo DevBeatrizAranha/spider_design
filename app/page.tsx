c69c362c011540078f8beda9ae03ab88d3e485b1
Beatriz <bmja.dev@gmail.com>
2024-07-09 17:52:42 -0300

    Configs de CSS e do canvas

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import { Room } from "./Room";
-import { CollaborativeApp } from "./CollaborativeApp";
 
 export default function Page() {
   return (
-    <Room>
-      <CollaborativeApp />
-    </Room>
+    
+      <h1 className="text-5xl text-white">Liveblocks figma clone</h1>
+   
   );
 }

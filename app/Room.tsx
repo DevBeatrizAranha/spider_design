2be5257c931f5b6808603dd018703708c79fdd78
Beatriz <bmja.dev@gmail.com>
2024-07-09 17:06:12 -0300

    Instalando e configurando dependências

---
+++ app/Room.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { ReactNode } from "react";
+import {
+  LiveblocksProvider,
+  RoomProvider,
+  ClientSideSuspense,
+} from "@liveblocks/react/suspense";
+
+export function Room({ children }: { children: ReactNode }) {
+  return (
+    <LiveblocksProvider publicApiKey={"pk_dev_5ap6O-29ZJkypGRhHcClc-NvQ_z5mud2eyYK4deWB3WU2utE0I9r_xJ3glNobakF"}>
+      <RoomProvider id="my-room">
+        <ClientSideSuspense fallback={<div>Loading…</div>}>
+          {children}
+        </ClientSideSuspense>
+      </RoomProvider>
+    </LiveblocksProvider>
+  );
+}

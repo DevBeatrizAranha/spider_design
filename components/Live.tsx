72a5c3effa7d3f9745f197ea482211ae1b0273a9
Beatriz <bmja.dev@gmail.com>
2024-07-10 13:54:58 -0300

    adicionando balão de chat no live cursor

--- components/Live.tsx
+++ components/Live.tsx
@@ -1,51 +1,65 @@
 import { useMyPresence, useOthers } from "@liveblocks/react"
 import LiveCursors from "./cursor/LiveCursors"
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
+import { CursorMode } from "@/types/type";
+import CursorChat from "./cursor/CursorChat";
 
 const Live = () => {
     const others = useOthers();
-    const [{cursor}, updateMyPresence] = useMyPresence() as any;
+    const [{ cursor }, updateMyPresence] = useMyPresence() as any;
+
+    const [cursorState, setCursorState] = useState({
+        mode: CursorMode.Hidden,
+    })
 
     const handlePointerMove = useCallback((event: React.PointerEvent) => {
         event.preventDefault();
-    
+
         const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
         const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
 
-        updateMyPresence({ cursor: { x, y} });
+        updateMyPresence({ cursor: { x, y } });
 
     }, []);
 
     const handlePointerLeave = useCallback((event: React.PointerEvent) => {
-        event.preventDefault();
-    
-
+        setCursorState({ mode: CursorMode.Hidden })
         updateMyPresence({ cursor: null, message: null });
 
     }, []);
 
     const handlePointerDown = useCallback((event: React.PointerEvent) => {
         event.preventDefault();
-    
+
         const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
         const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
 
-        updateMyPresence({ cursor: { x, y} });
+        updateMyPresence({ cursor: { x, y } });
 
     }, []);
 
-  return (
-    <div 
-    onPointerMove={handlePointerMove}
-    onPointerLeave={handlePointerLeave}
-    onPointerDown={handlePointerDown}
+    return (
+        <div
+            onPointerMove={handlePointerMove}
+            onPointerLeave={handlePointerLeave}
+            onPointerDown={handlePointerDown}
 
-    className="h-[100vh] w-full flex justify-center items-center text-center border-2 border-green-500"
-    >
-        <h1 className="text-5xl text-white">Liveblocks Figma Clone</h1>
-        <LiveCursors others={others}/>
-    </div>
-  )
+            className="h-[100vh] w-full flex justify-center items-center text-center border-2"
+        >
+            <h1 className="text-5xl text-white">Liveblocks Figma Clone</h1>
+
+            {cursor && (
+                <CursorChat
+                    cursor={cursor}
+                    cursorState={cursorState}
+                    setCursorState={setCursorState}
+                    updateMyPresence={updateMyPresence}
+                />
+            )}
+
+            <LiveCursors others={others} />
+        </div>
+    )
 }
 
 export default Live
--- components/cursor/CursorChat.tsx
+++ components/cursor/CursorChat.tsx
@@ -1,8 +1,34 @@
+import CursorSVG from '@/public/assets/CursorSVG'
+import { CursorChatProps, CursorMode } from '@/types/type'
 import React from 'react'
 
-const CursorChat = () => {
+  //  {cursorState.mode === CursorMode.Chat && (
+  // )}
+
+const CursorChat = ({ cursor, cursorState, setCursorState, updateMyPresence }: CursorChatProps) => {
   return (
-    <div>CursorChat</div>
+    <div className='absolute top-0 left-0' style={{
+      transform: `translatex(${cursor.x}px) translateY(${cursor.y}px)`
+    }}>
+   
+        <>
+          <CursorSVG color='#000' />
+
+          <div className='absolute left-2 top-5 bg-blue-500 px-4 py-2 text-sm leading-relaxed text-white rounded-[20px]'>
+
+            {cursorState.previousMessage && (
+              <div>{cursorState.previousMessage}</div>
+            )}
+            <input className='z-10 w-60 border-none bg-transparent text-white placeholder-blue-300 outline-none'
+            autoFocus={true}
+            />
+
+          </div>
+
+
+        </>
+      
+    </div>
   )
 }
 
